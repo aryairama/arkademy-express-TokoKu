d8b06aaa5f56864ed1d9ccb4e3a49fc075ee29f5
Arya Irama Wahono <aryairama987@gmail.com>
2021-09-13 16:38:50 +0700

    add option port in db connection

--- src/configs/db.js
+++ src/configs/db.js
@@ -5,6 +5,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   multipleStatements: true,
 });
 
